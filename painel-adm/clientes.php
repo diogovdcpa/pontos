81d81ce535a543c904e90f8d9ce2548524b9291f
Diogo Caldas <diogovdcpa@gmail.com>
2021-02-14 16:09:15 -0300

    Listando dados clientes

--- painel-adm/clientes.php
+++ painel-adm/clientes.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 $pag = "clientes";
-require_once("../conexao.php"); 
+require_once("../conexao.php");
 
 /*
 @session_start();
@@ -13,20 +13,21 @@ if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'Admin'){
 
 ?>
 
+<!-- BOTAO NOVO -->
 <div class="row mt-4 mb-4">
     <a type="button" class="btn-primary btn-sm ml-3 d-none d-md-block" href="index.php?pag=<?php echo $pag ?>&funcao=novo">Novo Cliente</a>
     <a type="button" class="btn-primary btn-sm ml-3 d-block d-sm-none" href="index.php?pag=<?php echo $pag ?>&funcao=novo">+</a>
-    
 </div>
 
 
 
-<!-- DataTales Example -->
+<!-- DataTales  -->
 <div class="card shadow mb-4">
 
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <!-- HEADER DA TABELA -->
                 <thead>
                     <tr>
                         <th>Inova</th>
@@ -36,39 +37,39 @@ if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'Admin'){
                     </tr>
                 </thead>
 
+                <!-- BODY DA TABELA -->
                 <tbody>
 
-                   <?php 
+                    <!-- BUSCA NO BANCO DE DADOS -->
+                    <?php
 
-                   $query = $pdo->query("SELECT * FROM clientes order by id desc ");
-                   $res = $query->fetchAll(PDO::FETCH_ASSOC);
+                    $query = $pdo->query("SELECT * FROM clientes order by id desc ");
+                    $res = $query->fetchAll(PDO::FETCH_ASSOC);
 
-                   for ($i=0; $i < count($res); $i++) { 
-                      foreach ($res[$i] as $key => $value) {
-                      }
+                    for ($i = 0; $i < count($res); $i++) {
+                        foreach ($res[$i] as $key => $value) {
+                        }
 
-                      $id = $res[$i]['id'];
-                      $nome = $res[$i]['inova'];
-                      $nome = $res[$i]['nome'];
-                      $nome = $res[$i]['telefone'];
-                      
-                      ?>
+                        // SALVANDO VALORES NAS VARIARES
+                        $id = $res[$i]['id'];
+                        $inova = $res[$i]['inova'];
+                        $nome = $res[$i]['nome'];
+                        $telefone = $res[$i]['telefone'];
 
+                    ?>
 
-                    <tr>
-                        <td><?php echo $nome ?></td>
-                       
-
-                        <td>
-                             <a href="index.php?pag=<?php echo $pag ?>&funcao=editar&id=<?php echo $id ?>" class='text-primary mr-1' title='Editar Dados'><i class='far fa-edit'></i></a>
-                            <a href="index.php?pag=<?php echo $pag ?>&funcao=excluir&id=<?php echo $id ?>" class='text-danger mr-1' title='Excluir Registro'><i class='far fa-trash-alt'></i></a>
-                        </td>
-                    </tr>
-<?php } ?>
-
-
-
-
+                        <!-- EXIBINDO DADOS NA TABELA -->
+                        <tr>
+                            <td><?php echo $inova ?></td>
+                            <td><?php echo $nome ?></td>
+                            <td><?php echo $telefone ?></td>
+                            <!-- BOTOES DE AÇÃO -->
+                            <td>
+                                <a href="index.php?pag=<?php echo $pag ?>&funcao=editar&id=<?php echo $id ?>" class='text-primary mr-1' title='Editar Dados'><i class='far fa-edit'></i></a>
+                                <a href="index.php?pag=<?php echo $pag ?>&funcao=excluir&id=<?php echo $id ?>" class='text-danger mr-1' title='Excluir Registro'><i class='far fa-trash-alt'></i></a>
+                            </td>
+                        </tr>
+                    <?php } ?>
 
                 </tbody>
             </table>
@@ -77,15 +78,12 @@ if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'Admin'){
 </div>
 
 
-
-
-
-<!-- Modal -->
+<!-- MODAL INSERIR E EDITAR -->
 <div class="modal fade" id="modalDados" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <?php 
+                <?php
                 if (@$_GET['funcao'] == 'editar') {
                     $titulo = "Editar Registro";
                     $id2 = $_GET['id'];
@@ -94,16 +92,13 @@ if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'Admin'){
                     $res = $query->fetchAll(PDO::FETCH_ASSOC);
 
                     $nome2 = $res[0]['nome'];
-                                                            
-
                 } else {
                     $titulo = "Inserir Registro";
-
                 }
 
 
                 ?>
-                
+
                 <h5 class="modal-title" id="exampleModalLabel"><?php echo $titulo ?></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
@@ -113,18 +108,18 @@ if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'Admin'){
                 <div class="modal-body">
 
                     <div class="form-group">
-                        <label >Nome</label>
+                        <label>Nome</label>
                         <input value="<?php echo @$nome2 ?>" type="text" class="form-control" id="nome-cat" name="nome-cat" placeholder="Nome">
                     </div>
 
-                  
-                   
+
+
 
                     <small>
                         <div id="mensagem">
 
                         </div>
-                    </small> 
+                    </small>
 
                 </div>
 
@@ -134,8 +129,8 @@ if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'Admin'){
 
 
 
-                <input value="<?php echo @$_GET['id'] ?>" type="hidden" name="txtid2" id="txtid2">
-                <input value="<?php echo @$nome2 ?>" type="hidden" name="antigo" id="antigo">
+                    <input value="<?php echo @$_GET['id'] ?>" type="hidden" name="txtid2" id="txtid2">
+                    <input value="<?php echo @$nome2 ?>" type="hidden" name="antigo" id="antigo">
 
                     <button type="button" id="btn-fechar" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
                     <button type="submit" name="btn-salvar" id="btn-salvar" class="btn btn-primary">Salvar</button>
@@ -146,10 +141,7 @@ if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'Admin'){
 </div>
 
 
-
-
-
-
+<!-- MODAL DELETAR -->
 <div class="modal" id="modal-deletar" tabindex="-1" role="dialog">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -172,7 +164,7 @@ if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'Admin'){
                 <button type="button" class="btn btn-secondary" data-dismiss="modal" id="btn-cancelar-excluir">Cancelar</button>
                 <form method="post">
 
-                    <input type="hidden" id="id"  name="id" value="<?php echo @$_GET['id'] ?>" required>
+                    <input type="hidden" id="id" name="id" value="<?php echo @$_GET['id'] ?>" required>
 
                     <button type="button" id="btn-deletar" name="btn-deletar" class="btn btn-danger">Excluir</button>
                 </form>
@@ -182,10 +174,8 @@ if(@$_SESSION['id_usuario'] == null || @$_SESSION['nivel_usuario'] != 'Admin'){
 </div>
 
 
-
-
-
-<?php 
+<!-- VERIFICA A CONDIÇAO DO GET -->
+<?php
 
 if (@$_GET["funcao"] != null && @$_GET["funcao"] == "novo") {
     echo "<script>$('#modalDados').modal('show');</script>";
@@ -202,12 +192,10 @@ if (@$_GET["funcao"] != null && @$_GET["funcao"] == "excluir") {
 ?>
 
 
-
-
 <!--AJAX PARA INSERÇÃO E EDIÇÃO DOS DADOS COM IMAGEM -->
 <script type="text/javascript">
-    $("#form").submit(function () {
-        var pag = "<?=$pag?>";
+    $("#form").submit(function() {
+        var pag = "<?= $pag ?>";
         event.preventDefault();
         var formData = new FormData(this);
 
@@ -216,16 +204,16 @@ if (@$_GET["funcao"] != null && @$_GET["funcao"] == "excluir") {
             type: 'POST',
             data: formData,
 
-            success: function (mensagem) {
+            success: function(mensagem) {
 
                 $('#mensagem').removeClass()
 
                 if (mensagem.trim() == "Salvo com Sucesso!!") {
-                    
+
                     //$('#nome').val('');
                     //$('#cpf').val('');
                     $('#btn-fechar').click();
-                    window.location = "index.php?pag="+pag;
+                    window.location = "index.php?pag=" + pag;
 
                 } else {
 
@@ -239,10 +227,10 @@ if (@$_GET["funcao"] != null && @$_GET["funcao"] == "excluir") {
             cache: false,
             contentType: false,
             processData: false,
-            xhr: function () {  // Custom XMLHttpRequest
+            xhr: function() { // Custom XMLHttpRequest
                 var myXhr = $.ajaxSettings.xhr();
                 if (myXhr.upload) { // Avalia se tem suporte a propriedade upload
-                    myXhr.upload.addEventListener('progress', function () {
+                    myXhr.upload.addEventListener('progress', function() {
                         /* faz alguma coisa durante o progresso do upload */
                     }, false);
                 }
@@ -253,14 +241,11 @@ if (@$_GET["funcao"] != null && @$_GET["funcao"] == "excluir") {
 </script>
 
 
-
-
-
 <!--AJAX PARA EXCLUSÃO DOS DADOS -->
 <script type="text/javascript">
-    $(document).ready(function () {
-        var pag = "<?=$pag?>";
-        $('#btn-deletar').click(function (event) {
+    $(document).ready(function() {
+        var pag = "<?= $pag ?>";
+        $('#btn-deletar').click(function(event) {
             event.preventDefault();
 
             $.ajax({
@@ -268,7 +253,7 @@ if (@$_GET["funcao"] != null && @$_GET["funcao"] == "excluir") {
                 method: "post",
                 data: $('form').serialize(),
                 dataType: "text",
-                success: function (mensagem) {
+                success: function(mensagem) {
 
                     if (mensagem.trim() === 'Excluído com Sucesso!!') {
 
@@ -289,17 +274,15 @@ if (@$_GET["funcao"] != null && @$_GET["funcao"] == "excluir") {
 </script>
 
 
-
 <!--SCRIPT PARA CARREGAR IMAGEM -->
 <script type="text/javascript">
-
     function carregarImg() {
 
         var target = document.getElementById('target');
         var file = document.querySelector("input[type=file]").files[0];
         var reader = new FileReader();
 
-        reader.onloadend = function () {
+        reader.onloadend = function() {
             target.src = reader.result;
         };
 
@@ -311,21 +294,14 @@ if (@$_GET["funcao"] != null && @$_GET["funcao"] == "excluir") {
             target.src = "";
         }
     }
-
 </script>
 
 
-
-
-
 <script type="text/javascript">
-    $(document).ready(function () {
+    $(document).ready(function() {
         $('#dataTable').dataTable({
             "ordering": false
         })
 
     });
 </script>
-
-
-
